090864e1866e232cf815ce4ba14f9a60f376380b
Chinmay G Khasnis <chinmaykhasanis@gmail.com>
2021-03-08 12:09:21 +0530

    Compatible with TF 2.x

--- bayes_filter.py
+++ bayes_filter.py
@@ -41,21 +41,21 @@ class BayesFilter():
                 prev_size = args.state_dim
             else:
                 prev_size = args.extractor_size[i-1]
-            self.extractor_w.append(tf.Variable("extractor_w"+str(i), [prev_size, args.extractor_size[i]], 
-                                                regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight)))
-            self.extractor_b.append(tf.Variable("extractor_b"+str(i), [args.extractor_size[i]]))
+            self.extractor_w.append(tf.compat.v1.get_variable("extractor_w"+str(i), [prev_size, args.extractor_size[i]], 
+                                                regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight))))
+            self.extractor_b.append(tf.compat.v1.get_variable("extractor_b"+str(i), [args.extractor_size[i]]))
 
         # Last set of weights to map to output
-        self.extractor_w.append(tf.Variable("extractor_w_end", [args.extractor_size[-1], args.code_dim], 
-                                                regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight)))
-        self.extractor_b.append(tf.Variable("extractor_b_end", [args.code_dim]))
+        self.extractor_w.append(tf.compat.v1.get_variable("extractor_w_end", [args.extractor_size[-1], args.code_dim], 
+                                                regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight))))
+        self.extractor_b.append(tf.compat.v1.get_variable("extractor_b_end", [args.code_dim]))
 
     # Function to run inputs through extractor
     def _get_extractor_output(self, args, states):
         extractor_input = states
         for i in range(len(args.extractor_size)):
-            extractor_input = tf.nn.relu(tf.nn.xw_plus_b(extractor_input, self.extractor_w[i], self.extractor_b[i]))
-        output = tf.nn.xw_plus_b(extractor_input, self.extractor_w[-1], self.extractor_b[-1])
+            extractor_input = tf.nn.relu(tf.compat.v1.nn.xw_plus_b(extractor_input, self.extractor_w[i], self.extractor_b[i]))
+        output = tf.compat.v1.nn.xw_plus_b(extractor_input, self.extractor_w[-1], self.extractor_b[-1])
         return output
 
     # Create feature extractor (maps state -> features, assumes feature same dimensionality as latent states)
@@ -67,53 +67,53 @@ class BayesFilter():
     def _gen_sample(self, args, dist_params):
         w_mean, w_logstd = tf.split(dist_params, [args.noise_dim, args.noise_dim], axis=1)
         w_std = tf.exp(w_logstd) + 1e-3
-        samples = tf.random_normal([args.batch_size, args.noise_dim])
+        samples = tf.random.normal([args.batch_size, args.noise_dim])
         w = samples*w_std + w_mean
         w = tf.minimum(tf.maximum(w, -10.0), 10.0)
-        w = tf.cond(self.generative, lambda: samples, lambda: w)  # Just sample from prior for generative model
+        w = tf.cond(pred=self.generative, true_fn=lambda: samples, false_fn=lambda: w)  # Just sample from prior for generative model
         return w
 
     # Bidirectional LSTM to generate initial sample of w1, then form z1 from w1
     def _create_initial_generator(self, args):
-        fwd_cell = tf.nn.rnn_cell.LSTMCell(args.rnn_size, initializer=tf.contrib.layers.xavier_initializer())
-        bwd_cell = tf.nn.rnn_cell.LSTMCell(args.rnn_size, initializer=tf.contrib.layers.xavier_initializer())
+        fwd_cell = tf.compat.v1.nn.rnn_cell.LSTMCell(args.rnn_size, initializer=tf.compat.v1.keras.initializers.VarianceScaling(scale=1.0, mode="fan_avg", distribution="uniform"))
+        bwd_cell = tf.compat.v1.nn.rnn_cell.LSTMCell(args.rnn_size, initializer=tf.compat.v1.keras.initializers.VarianceScaling(scale=1.0, mode="fan_avg", distribution="uniform"))
         
         # Get outputs from rnn and concatenate
-        outputs, _ = tf.nn.bidirectional_dynamic_rnn(fwd_cell, bwd_cell, self.features[:, :args.seq_length], dtype=tf.float32)
+        outputs, _ = tf.compat.v1.nn.bidirectional_dynamic_rnn(fwd_cell, bwd_cell, self.features[:, :args.seq_length], dtype=tf.float32)
         output_fw, output_bw = outputs
         output = tf.concat([output_fw[:, -1], output_bw[:, -1]], axis=1)
 
         # Single affine transformation into w1 distribution params
-        hidden = tf.layers.dense(output, 
+        hidden = tf.compat.v1.layers.dense(output, 
                                 args.transform_size, 
                                 activation=tf.nn.relu,
                                 name='to_hidden_w1', 
-                                kernel_regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight))
-        self.w1_dist = tf.layers.dense(hidden, 
+                                kernel_regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight)))
+        self.w1_dist = tf.compat.v1.layers.dense(hidden, 
                                 2*args.noise_dim, 
                                 name='to_w1', 
-                                kernel_regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight))
+                                kernel_regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight)))
         self.w1 = self._gen_sample(args, self.w1_dist)
 
         # Now construct z1 through transformation with single hidden layer
-        hidden = tf.layers.dense(self.w1, 
+        hidden = tf.compat.v1.layers.dense(self.w1, 
                                 args.transform_size, 
                                 activation=tf.nn.relu,
                                 name='to_hidden_z1', 
-                                kernel_regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight))
-        self.z1 = tf.layers.dense(hidden, 
+                                kernel_regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight)))
+        self.z1 = tf.compat.v1.layers.dense(hidden, 
                                 args.code_dim, 
                                 name='to_z1', 
-                                kernel_regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight))
+                                kernel_regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight)))
 
     # Initialize potential transition matrices
     def _create_transition_matrices(self, args):
-        self.A_matrices = tf.Variable("A_matrices", [args.num_matrices, args.code_dim, args.code_dim], 
-                                            regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight))
-        self.B_matrices = tf.Variable("B_matrices", [args.num_matrices, args.action_dim, args.code_dim], 
-                                            regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight))
-        self.C_matrices = tf.Variable("C_matrices", [args.num_matrices, args.noise_dim, args.code_dim], 
-                                            regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight))
+        self.A_matrices = tf.compat.v1.get_variable("A_matrices", [args.num_matrices, args.code_dim, args.code_dim], 
+                                            regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight)))
+        self.B_matrices = tf.compat.v1.get_variable("B_matrices", [args.num_matrices, args.action_dim, args.code_dim], 
+                                            regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight)))
+        self.C_matrices = tf.compat.v1.get_variable("C_matrices", [args.num_matrices, args.noise_dim, args.code_dim], 
+                                            regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight)))
 
     # Create parameters to comprise weight network
     def _create_weight_network_params(self, args):
@@ -121,15 +121,15 @@ class BayesFilter():
         self.weight_b = []
 
         # Have single hidden layer and fully connected to output
-        self.weight_w.append(tf.Variable("weight_w1", [args.code_dim+args.action_dim, args.transform_size], 
-                                            regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight)))
-        self.weight_w.append(tf.Variable("weight_w2", [args.transform_size, args.num_matrices], 
-                                            regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight)))
+        self.weight_w.append(tf.compat.v1.get_variable("weight_w1", [args.code_dim+args.action_dim, args.transform_size], 
+                                            regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight))))
+        self.weight_w.append(tf.compat.v1.get_variable("weight_w2", [args.transform_size, args.num_matrices], 
+                                            regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight))))
 
-        self.weight_b.append(tf.Variable("weight_b1", [args.transform_size], 
-                                            regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight)))
-        self.weight_b.append(tf.Variable("weight_b2", [args.num_matrices], 
-                                            regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight)))
+        self.weight_b.append(tf.compat.v1.get_variable("weight_b1", [args.transform_size], 
+                                            regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight))))
+        self.weight_b.append(tf.compat.v1.get_variable("weight_b2", [args.num_matrices], 
+                                            regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight))))
 
     # Create parameters to comprise inference network
     def _create_inference_network_params(self, args):
@@ -142,27 +142,27 @@ class BayesFilter():
                 prev_size = args.feature_dim+args.code_dim+args.action_dim
             else:
                 prev_size = args.inference_size[i-1]
-            self.inference_w.append(tf.Variable("inference_w"+str(i), [prev_size, args.inference_size[i]], 
-                                                regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight)))
-            self.inference_b.append(tf.Variable("inference_b"+str(i), [args.inference_size[i]]))
+            self.inference_w.append(tf.compat.v1.get_variable("inference_w"+str(i), [prev_size, args.inference_size[i]], 
+                                                regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight))))
+            self.inference_b.append(tf.compat.v1.get_variable("inference_b"+str(i), [args.inference_size[i]]))
 
         # Last set of weights to map to output
-        self.inference_w.append(tf.Variable("inference_w_end", [args.inference_size[-1], 2*args.noise_dim], 
-                                                regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight)))
-        self.inference_b.append(tf.Variable("inference_b_end", [2*args.noise_dim]))
+        self.inference_w.append(tf.compat.v1.get_variable("inference_w_end", [args.inference_size[-1], 2*args.noise_dim], 
+                                                regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight))))
+        self.inference_b.append(tf.compat.v1.get_variable("inference_b_end", [2*args.noise_dim]))
 
     # Function to get weights for transition matrices
     def _get_weights(self, z, u):
         z_u = tf.concat([z, u], axis=1)
-        hidden = tf.nn.relu(tf.nn.xw_plus_b(z_u, self.weight_w[0], self.weight_b[0]))
-        return tf.nn.softmax(tf.nn.xw_plus_b(hidden, self.weight_w[1], self.weight_b[1]))
+        hidden = tf.nn.relu(tf.compat.v1.nn.xw_plus_b(z_u, self.weight_w[0], self.weight_b[0]))
+        return tf.nn.softmax(tf.compat.v1.nn.xw_plus_b(hidden, self.weight_w[1], self.weight_b[1]))
 
     # Function to generate w sample from inference network
     def _get_inference_sample(self, args, features, z, u):
         inference_input = tf.concat([features, z, u], axis=1)
         for i in range(len(args.inference_size)):
-            inference_input = tf.nn.relu(tf.nn.xw_plus_b(inference_input, self.inference_w[i], self.inference_b[i]))
-        w_dist = tf.nn.xw_plus_b(inference_input, self.inference_w[-1], self.inference_b[-1])
+            inference_input = tf.nn.relu(tf.compat.v1.nn.xw_plus_b(inference_input, self.inference_w[i], self.inference_b[i]))
+        w_dist = tf.compat.v1.nn.xw_plus_b(inference_input, self.inference_w[-1], self.inference_b[-1])
 
         # Generate sample
         w = self._gen_sample(args, w_dist)
@@ -221,7 +221,7 @@ class BayesFilter():
             C_t = tf.stack(C_t_list) 
 
             # Draw noise sample and append sample to list
-            w_t = tf.random_normal([args.batch_size, args.noise_dim])
+            w_t = tf.random.normal([args.batch_size, args.noise_dim])
 
             # Now advance observation forward in time
             u_t = tf.expand_dims(u_t, axis=1)
@@ -243,28 +243,28 @@ class BayesFilter():
                 prev_size = args.code_dim
             else:
                 prev_size = args.extractor_size[i+1]
-            self.decoder_w.append(tf.Variable("decoder_w"+str(len(args.extractor_size)-i), [prev_size, args.extractor_size[i]], 
-                                                regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight)))
-            self.decoder_b.append(tf.Variable("decoder_b"+str(len(args.extractor_size)-i), [args.extractor_size[i]]))
+            self.decoder_w.append(tf.compat.v1.get_variable("decoder_w"+str(len(args.extractor_size)-i), [prev_size, args.extractor_size[i]], 
+                                                regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight))))
+            self.decoder_b.append(tf.compat.v1.get_variable("decoder_b"+str(len(args.extractor_size)-i), [args.extractor_size[i]]))
 
         # Last set of weights to map to output
-        self.decoder_w.append(tf.Variable("decoder_w_end", [args.extractor_size[0], args.state_dim], 
-                                                regularizer=tf.contrib.layers.l2_regularizer(args.reg_weight)))
-        self.decoder_b.append(tf.Variable("decoder_b_end", [args.state_dim]))
+        self.decoder_w.append(tf.compat.v1.get_variable("decoder_w_end", [args.extractor_size[0], args.state_dim], 
+                                                regularizer=tf.keras.regularizers.l2(0.5 * (args.reg_weight))))
+        self.decoder_b.append(tf.compat.v1.get_variable("decoder_b_end", [args.state_dim]))
 
     # Function to run inputs through decoder
     def _get_decoder_output(self, args, encodings):
         decoder_input = encodings
         for i in range(len(args.extractor_size)):
-            decoder_input = tf.nn.relu(tf.nn.xw_plus_b(decoder_input, self.decoder_w[i], self.decoder_b[i]))
-        output = tf.nn.xw_plus_b(decoder_input, self.decoder_w[-1], self.decoder_b[-1])
+            decoder_input = tf.nn.relu(tf.compat.v1.nn.xw_plus_b(decoder_input, self.decoder_w[i], self.decoder_b[i]))
+        output = tf.compat.v1.nn.xw_plus_b(decoder_input, self.decoder_w[-1], self.decoder_b[-1])
         return output
 
     # Create optimizer to minimize loss
     def _create_optimizer(self, args):
         # Find reconstruction loss
         self.rec_sol = self._get_decoder_output(args, self.z_pred)
-        self.loss_reconstruction = tf.reduce_sum(tf.square(self.x - self.rec_sol))
+        self.loss_reconstruction = tf.reduce_sum(input_tensor=tf.square(self.x - self.rec_sol))
 
         # Find state predictions by undoing data normalization
         self.state_pred = self.rec_sol*self.scale + self.shift
@@ -274,18 +274,25 @@ class BayesFilter():
         w_std = tf.exp(w_logstd) + 1e-3
 
         # Define distribution and prior objects
-        w_dist = tf.distributions.Normal(loc=w_mean, scale=w_std)
-        prior_dist = tf.distributions.Normal(loc=tf.zeros_like(w_mean), scale=tf.ones_like(w_std))
-        self.kl_loss = tf.reduce_sum(tf.distributions.kl_divergence(w_dist, prior_dist))
+        w_dist = tf.compat.v1.distributions.Normal(loc=w_mean, scale=w_std)
+        prior_dist = tf.compat.v1.distributions.Normal(loc=tf.zeros_like(w_mean), scale=tf.ones_like(w_std))
+        self.kl_loss = tf.reduce_sum(input_tensor=tf.compat.v1.distributions.kl_divergence(w_dist, prior_dist))
 
         # Sum with regularization losses to form total cost
-        self.cost = self.loss_reconstruction + tf.reduce_sum(tf.losses.get_regularization_losses()) + self.kl_weight*self.kl_loss
+        self.cost = self.loss_reconstruction + self.kl_weight*self.kl_loss + tf.reduce_sum(input_tensor=tf.compat.v1.losses.get_regularization_losses()) 
 
         # Perform parameter update
-        optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        tvars = tf.trainable_variables()
-        self.grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars), args.grad_clip)
-        self.train = optimizer.apply_gradients(zip(self.grads, tvars))
+        optimizer = tf.compat.v1.train.AdamOptimizer(self.learning_rate)
+        tvars = tf.compat.v1.trainable_variables()
+        
+        with tf.GradientTape() as tape:
+            loss_value =  self.cost
+        
+        self.grads, _ = tf.clip_by_global_norm(tape.gradient(loss_value, tf.compat.v1.trainable_variables()), args.grad_clip)
+        #self.grads = tape.gradient(loss_value, tvars)
+
+        
+        self.train = optimizer.apply_gradients(zip(self.grads, tf.compat.v1.trainable_variables()))
 
 
 
